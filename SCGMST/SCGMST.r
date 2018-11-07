714a32685c1190ac8755eb24f55a971e375c6a82
ceausuio <ceausuio@hu-berlin.de>
2018-11-07 17:03:51 +0100

    Update MST

---
+++ SCGMST/SCGMST.r
@@ -0,0 +1,25 @@
+# Packages and libraries needed -------------------------------------------
+
+library("MASS")
+require(stats)
+library(igraph)
+
+df = Boston
+#plot(df$dis, df$medv)
+d <- dist(cbind(df$dis, df$medv))
+matrix_of_distances <- as.matrix(d)
+G <- graph.adjacency(as.matrix(d), weighted = TRUE)
+
+## Some graphical parameters
+## MST and plot
+mst <- minimum.spanning.tree(G)
+edgelist = matrix(data = as.integer(igraph::get.edgelist(mst)), ncol = 2, byrow = F)
+
+df = Boston[, c('dis', 'medv')]
+
+plot(df, pch = 16, col = "blue")
+for (i in 1:dim(edgelist)[1]){
+  from = edgelist[i, 1]
+  to = edgelist[i, 2]
+  lines(df[c(from, to), 'dis'], df[c(from, to), 'medv'])
+}
