446dd579c1e4f0cb8f908ab982d0c154ae931e92
ceausuio <ceausuio@hu-berlin.de>
2019-04-16 13:12:19 +0200

    MST updates

--- SCGMST/SCGMST_startups.R
+++ SCGMST/SCGMST_startups.R
@@ -8,12 +8,11 @@ library(readxl)
 #EDP_10years = read_excel("/Users/ceausuio/SCG_Startups/20190403_EDP_short_startups.xlsx")
 
 #View(EDP_10years)
-
-
+#high stringy coefficient
 
 edp = as.data.frame(EDP_10years)
-stringy = cbind(edp$fins_wg_m1, edp$inv_pln_oe_3y)
-d = dist(cbind(edp$fins_wg_m1, edp$inv_pln_oe_3y))
+stringy = cbind(edp$inv_om_sf, edp$inv_pln_oe_3y)
+d = dist(cbind(edp$inv_om_sf, edp$inv_pln_oe_3y))
 
 dev.off()
 par(bg = NA)
@@ -26,19 +25,48 @@ G = graph.adjacency(as.matrix(d), weighted = TRUE)
 mst = minimum.spanning.tree(G)
 edgelist = matrix(data = as.integer(igraph::get.edgelist(mst)), ncol = 2, byrow = F)
 
-df = edp[, c('fins_wg_m1', 'inv_pln_oe_3y')]
+df = edp[, c('inv_om_sf', 'inv_pln_oe_3y')]
 
 par(bg = NA)
 plot(df, pch = 16, col = "blue")
 for (i in 1:dim(edgelist)[1]){
   from = edgelist[i, 1]
   to = edgelist[i, 2]
-  lines(df[c(from, to), 'fins_wg_m1'], df[c(from, to), 'inv_pln_oe_3y'])
+  lines(df[c(from, to), 'inv_om_sf'], df[c(from, to), 'inv_pln_oe_3y'])
 }
 
 boxplot(d)
 
-#########
+#############
+
+#low stringy coefficient
+stringy2 = cbind(edp$fins_profit_m1, edp$inv_pln_oe_3y)
+d = dist(cbind(edp$fins_profit_m1, edp$inv_pln_oe_3y))
+
+dev.off()
+par(bg = NA)
+plot(stringy, col = "dark blue")
+
+
+
+matrix_of_distances <- as.matrix(d)
+G = graph.adjacency(as.matrix(d), weighted = TRUE)
+mst = minimum.spanning.tree(G)
+edgelist = matrix(data = as.integer(igraph::get.edgelist(mst)), ncol = 2, byrow = F)
+
+df = edp[, c('fins_profit_m1', 'inv_pln_oe_3y')]
+
+par(bg = NA)
+plot(df, pch = 16, col = "blue")
+for (i in 1:dim(edgelist)[1]){
+  from = edgelist[i, 1]
+  to = edgelist[i, 2]
+  lines(df[c(from, to), 'fins_profit_m1'], df[c(from, to), 'inv_pln_oe_3y'])
+}
+
+boxplot(d)
+
+ #########
 
 
 sparse = cbind(edp$strup_age, edp$inv_pln_oe_3y)
